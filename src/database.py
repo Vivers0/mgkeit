d73fb4d3a7b03b7f41c16ba3eafa0181cf9d1e0d
Vivers0 <vivers.savva@gmail.com>
2020-12-22 17:15:39 +0300

    started create error_handler

--- src/database.py
+++ src/database.py
@@ -15,66 +15,71 @@ class DB:
         self.cursor = self.connect.cursor()
         self.send = Send(self.bot, self.cursor, self.timer)
 
+
     ################
     #
     # Autorisation
     #
     ################
 
-    def check_user_in_db(self, message):
-        user_id = message.from_user.id
-        res = 'SELECT * FROM users WHERE id = ?'
-        self.cursor.execute(res, [user_id,])
-        if (len(self.cursor.fetchall()) == 0):
-            return False
-        else:
-            return True
-        self.connect.commit()
+   
 
     def added_user_in_db(self, message):
-        if self.check_user_in_db(message) == False:
+        try:
             user_id = message.from_user.id
-            self.cursor.execute('INSERT INTO users (id) VALUES (?)', [user_id,])
+            self.cursor.execute('SELECT * FROM users WHERE id = ?', [user_id,])
+            if (len(self.cursor.fetchall()) == 0):             
+                user_id = message.from_user.id
+                self.cursor.execute('INSERT INTO users (id) VALUES (?)', [user_id,])
+                self.keybutton.send_build(message)
+            else:
+                self.bot.reply_to(message, 'Ты уже есть в базе данных 😊')
+        except:
+            self.bot.reply_to(message, 'Что-то пошло не так, попробуй еще раз зарегистрироваться - /start')
+        finally:
             self.connect.commit()
-            self.keybutton.send_build(message)
-            return True
-        else:
-            self.bot.reply_to(message, 'Ты уже есть в базе данных 😊')
 
     def added_user_course(self, message):
-        def get_build_user(id):
-            self.cursor.execute('SELECT build_id FROM users WHERE id = ?', [id,])
-            return self.cursor.fetchone()[0]
-        msg = message.text
-        user_id = message.from_user.id
-        course_id = self.course.get_course_id(msg, get_build_user(user_id))
-        if course_id == 0:
-            msg = self.bot.send_message(user_id, 'Я не нашел такого курса 🙁. Попробуй еще раз:)')
-            self.bot.register_next_step_handler(msg, self.added_user_course)
-        else:
-            self.cursor.execute('UPDATE users SET course_id = ? WHERE id = ?', [course_id, user_id,])
-            self.connect.commit()
-            self.bot.send_message(user_id, 'Отлично! Если ты хочешь получать уведомления с расписанием каждый день в 7 утра, то просто напиши /notify или нажми на этот текст:)')
-            self.keybutton.send_menu(message)
-
-    def added_user_build_id(self, call):
-        if call.message:
-            if call.data == 'mill':
-                user_id = call.message.chat.id
-                res = 'UPDATE users SET build_id = ? WHERE id = ?'
-                self.cursor.execute(res, ['mill', user_id,])
-                self.connect.commit()
-                msg = self.bot.send_message(call.message.chat.id, 'Отлично! ☺️ Теперь тебе нужно указать свой курс (Например: 1CА-1-20 или 3ЭК-18)')
-                self.bot.register_next_step_handler(msg, self.added_user_course)
-            elif call.data == 'sudo':
-                user_id = call.message.chat.id
-                res = 'UPDATE users SET build_id = ? WHERE id = ?'
-                self.cursor.execute(res, ['sudo', user_id,])
-                self.connect.commit()
-                self.bot.send_message(call.message.chat.id, 'Отлично! ☺️ Теперь тебе нужно указать свой курс (Например: 1CА-1-20 или 3ЭК-18)')
+        try:
+            def get_build_user(id):
+                self.cursor.execute('SELECT build_id FROM users WHERE id = ?', [id,])
+                return self.cursor.fetchone()[0]
+            msg = message.text
+            user_id = message.from_user.id
+            course_id = self.course.get_course_id(msg, get_build_user(user_id))
+            if course_id == 0:
+                msg = self.bot.send_message(user_id, 'Я не нашел такого курса 🙁. Попробуй еще раз:)')
                 self.bot.register_next_step_handler(msg, self.added_user_course)
             else:
-                self.bot.send_message(call.message.chat.id, 'Я не могу распознать корпус, котрорый 😣, попробуй еще раз!')
+                self.cursor.execute('UPDATE users SET course_id = ? WHERE id = ?', [course_id, user_id,])
+                self.bot.send_message(user_id, 'Отлично! Если ты хочешь получать уведомления с расписанием каждый день в 7 утра, то просто напиши /notify или нажми на этот текст:)')
+                self.keybutton.send_menu(message)
+        except:
+            self.bot.reply_to(message, 'Что-то пошло не так, попробуй еще раз зарегистрироваться - /start')
+        finally:
+            self.connect.commit()
+
+    def added_user_build_id(self, call):
+        try:
+            if call.message:
+                if call.data == 'mill':
+                    user_id = call.message.chat.id
+                    res = 'UPDATE users SET build_id = ? WHERE id = ?'
+                    self.cursor.execute(res, ['mill', user_id,])
+                    msg = self.bot.send_message(call.message.chat.id, 'Отлично! ☺️ Теперь тебе нужно указать свой курс (Например: 1CА-1-20 или 3ЭК-18)')
+                    self.bot.register_next_step_handler(msg, self.added_user_course)
+                elif call.data == 'sudo':
+                    user_id = call.message.chat.id
+                    res = 'UPDATE users SET build_id = ? WHERE id = ?'
+                    self.cursor.execute(res, ['sudo', user_id,])
+                    self.bot.send_message(call.message.chat.id, 'Отлично! ☺️ Теперь тебе нужно указать свой курс (Например: 1CА-1-20 или 3ЭК-18)')
+                    self.bot.register_next_step_handler(msg, self.added_user_course)
+                else:
+                    self.bot.send_message(call.message.chat.id, 'Я не могу распознать корпус, котрорый 😣, попробуй еще раз!')
+        except:
+            self.bot.reply_to(call.message, 'Что-то пошло не так, попробуй еще раз зарегистрироваться - /start')
+        finally:
+            self.connect.commit()
 
     def reset_user_in_db(self, message):
         user_id = message.from_user.id
@@ -91,17 +96,22 @@ class DB:
     ################
 
     def enable_notify(self, message):
-        user_id = message.from_user.id
-        self.cursor.execute('SELECT notify FROM users WHERE id = ?', [user_id,])
-        result = self.cursor.fetchone()
-        if result[0] == 'False':
-            self.cursor.execute('UPDATE users SET notify = ? WHERE id = ?', ['True', user_id,])
+        try:
+            user_id = message.from_user.id
+            self.cursor.execute('SELECT notify FROM users WHERE id = ?', [user_id,])
+            result = self.cursor.fetchone()
+            if result[0] == 'False':
+                self.cursor.execute('UPDATE users SET notify = ? WHERE id = ?', ['True', user_id,])
+                self.connect.commit()
+                self.bot.send_message(user_id, 'Автоматическая отправка расписания: *Включено*')
+            else:
+                self.cursor.execute('UPDATE users SET notify = ? WHERE id = ?', ['False', user_id,])
+                self.connect.commit()
+                self.bot.send_message(user_id, 'Автоматическая отправка расписания: *Выключено*')
+        except:
+            self.bot.send_message(user_id, 'Что-то пошло не так, попробуй еще раз зарегистрироваться - /start')
+        finally:
             self.connect.commit()
-            self.bot.send_message(user_id, 'Автоматическая отправка расписания: *Включено*')
-        else:
-            self.cursor.execute('UPDATE users SET notify = ? WHERE id = ?', ['False', user_id,])
-            self.connect.commit()
-            self.bot.send_message(user_id, 'Автоматическая отправка расписания: *Выключено*')
 
     def check_user_for_notify(self):
         user = dict()
@@ -118,9 +128,8 @@ class DB:
            self.cursor.execute('SELECT timetable FROM timetable WHERE course_id = ? AND day_week = ? AND is_odd = ?', [course, day_of_week(), is_eval(),])
            return self.cursor.fetchone()[0]
 
-            
         self.cursor.execute('SELECT * FROM users WHERE notify = ?', ['True',])
-
+        
         for t in self.cursor.fetchall():
             user[t[0]] = dict(timetable=get_timetable(t[1]), day=day_of_week())
         return user
@@ -160,22 +169,31 @@ class Send:
         self.timer = timer
 
     def today_button(self, id):
-        week = self.timer.get_weeklist()[int(self.today_of_week())]
-        evals = self.timer.get_eval()[int(self.is_eval()['num'])]
-        timetable = self.get_user_timetable(self.get_user_course(id), self.today_of_week(), self.is_eval()['word'])
-        self.bot.send_message(id, 'Расписание на ' + week + ' ('+ evals +')\n\n' + timetable)
+        try:
+            week = self.timer.get_weeklist()[int(self.today_of_week())]
+            evals = self.timer.get_eval()[int(self.is_eval()['num'])]
+            timetable = self.get_user_timetable(self.get_user_course(id), self.today_of_week(), self.is_eval()['word'])
+            self.bot.send_message(id, 'Расписание на ' + week + ' ('+ evals +')\n\n' + timetable)
+        except:
+            self.bot.send_message(id, 'Что-то пошло не так, попробуй еще раз зарегистрироваться - /start')
 
     def tomorrow_button(self, id):
-        week = self.timer.get_weeklist()[int(self.today_of_week())+1]
-        evals = self.timer.get_eval()[int(self.is_eval()['num'])]
-        timetable = self.get_user_timetable(self.get_user_course(id), self.today_of_week()+1, self.is_eval()['word'])
-        self.bot.send_message(id, 'Расписание на ' + week + ' ('+ evals +')\n\n' + timetable)
+        try:
+            week = self.timer.get_weeklist()[int(self.today_of_week())+1]
+            evals = self.timer.get_eval()[int(self.is_eval()['num'])]
+            timetable = self.get_user_timetable(self.get_user_course(id), self.today_of_week()+1, self.is_eval()['word'])
+            self.bot.send_message(id, 'Расписание на ' + week + ' ('+ evals +')\n\n' + timetable)
+        except:
+            self.bot.send_message(id, 'Что-то пошло не так, попробуй еще раз зарегистрироваться - /start')
 
     def another_day(self, id, day):
-        week = self.timer.get_weeklist()[day]
-        evals = self.timer.get_eval()[int(self.is_eval()['num'])]
-        timetable = self.get_user_timetable(self.get_user_course(id), day, self.is_eval()['word'])
-        self.bot.send_message(id, 'Расписание на ' + week + ' ('+ evals +')\n\n' + timetable)
+        try:
+            week = self.timer.get_weeklist()[day]
+            evals = self.timer.get_eval()[int(self.is_eval()['num'])]
+            timetable = self.get_user_timetable(self.get_user_course(id), day, self.is_eval()['word'])
+            self.bot.send_message(id, 'Расписание на ' + week + ' ('+ evals +')\n\n' + timetable)
+        except:
+            self.bot.send_message(id, 'Что-то пошло не так, попробуй еще раз зарегистрироваться - /start')
 
     # ---------------------------
 
@@ -191,6 +209,7 @@ class Send:
     def get_user_course(self, id):
         self.cursor.execute('SELECT course_id FROM users WHERE id = ?', [id,])
         return self.cursor.fetchone()[0]
+        self.bot.send_message(id, 'Что-то пошло не так, попробуй еще раз зарегистрироваться - /start')
 
     def get_user_timetable(self, course, day, eval):
         self.cursor.execute('SELECT timetable FROM timetable WHERE course_id = ? AND day_week = ? AND is_odd = ?', [course, day, eval,])
